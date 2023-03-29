8799618bbbb36bc850ecc4339984dc461e1a32b0
Don Yu <37088038+devzons@users.noreply.github.com>
2023-03-28 22:15:46 -0700

    Added router parameters

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,3 +1,15 @@
+import { useSearchParams, useLocation } from 'react-router-dom'
+
 export const ProductList = () => {
-  return <div className='component'>ProductList</div>
+  const [searchParams] = useSearchParams()
+  // console.log(searchParams.get('keyword'))
+
+  const location = useLocation()
+  // console.log(location)
+
+  return (
+    <div className='component'>
+      <p>ProductList</p>
+    </div>
+  )
 }
